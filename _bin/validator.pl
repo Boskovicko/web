be3cc4ab8cd164a632186ea171b99b8db6149fd5
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2013-11-27 15:27:46 +0100

    Detekce bílých znaků na koncích řádek a nepovolených znaků v názvech souborů.

--- _bin/validator.pl
+++ _bin/validator.pl
@@ -7,6 +7,7 @@ use lib $Bin;
 use Modern::Perl;
 use List::Util qw/any none/;
 use Text 'regex_find_all';
+use File::Basename 'basename';
 use Jekyll;
 
 my $root = shift @ARGV || '_posts';
@@ -28,6 +29,17 @@ my @rules = (
         condition => sub { shift->body =~ /\s+-\s+/ },
         message => "Rozdělovník místo pomlčky?",
         regex => qr/(?<=\s)-\s+/
+    }, {
+        condition => sub { shift->body =~ /^\s+$/m },
+        message => "Nadbytečné bílé znaky na řádku.",
+        regex => qr/^\s+$/
+    }, {
+        condition => sub { basename(shift->path) !~ /^[\w\-\.\/]+$/ },
+        message => "Nepovolené znaky v názvu souboru."
+    }, {
+        condition => sub { shift->body =~ /\s+$/ },
+        message => "Nadbytečné bílé znaky na konci řádku.",
+        regex => qr/\s+$/
     }
 );
 
@@ -35,7 +47,9 @@ for my $article ($jekyll->all_articles) {
     next unless $article->head->published;
     for my $rule (@rules) {
         next unless $rule->{condition}->($article);
-        my $occurences = regex_find_all($rule->{regex}, $article->source);
+        my $occurences = $rule->{regex} ?
+            regex_find_all($rule->{regex}, $article->source) :
+            [[0, 0]];
         for my $loc (@$occurences) {
             printf "%s:%i:%i:%s\n", $article->path, $loc->[0], $loc->[1], $rule->{message};
         }
