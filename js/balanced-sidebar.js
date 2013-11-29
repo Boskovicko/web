85687d8d3ea8611fc6c4cdedd25cde52e5f64481
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2013-11-29 15:32:14 +0100

    Krácení seznamu souvisejících článků podle délky hlavního obsahu.

---
+++ js/balanced-sidebar.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    var mainColumnHeight = $('.clanek').height();
+    for (;;) {
+        var MIN_RELATED_ARTICLES = 1;
+        var sidebarHeight = $('.sidebar').height();
+        var relatedArticleCount = $('.sidebar .clanek-nahled').length;
+        if (sidebarHeight > mainColumnHeight && relatedArticleCount > MIN_RELATED_ARTICLES) {
+            $('.sidebar .clanek-nahled:last-child').remove();
+        } else {
+            break;
+        }
+    }
+});
