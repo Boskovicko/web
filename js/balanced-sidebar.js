42ec32c4a1b3fe8f66ad3d48f345c65068192206
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2013-12-12 14:08:43 +0100

    Vyhodil jsem zapomenuté logovací výpisy.

--- js/balanced-sidebar.js
+++ js/balanced-sidebar.js
@@ -2,18 +2,13 @@ $(window).load(function() {
     var MIN_RELATED_ARTICLES = 1;
     var TOLERABLE_OVERHANG = 150;
     var mainColumnHeight = $('.clanek-telo').height();
-    console.log('Main column height: ' + mainColumnHeight);
     for (;;) {
         var sidebarHeight = $('.sidebar').height();
         var relatedArticleCount = $('.sidebar .clanek-nahled').length;
-        console.log('Sidebar: ' + sidebarHeight);
-        console.log('Related articles: ' + relatedArticleCount);
         if ((sidebarHeight > mainColumnHeight + TOLERABLE_OVERHANG)
             && (relatedArticleCount > MIN_RELATED_ARTICLES)) {
-            console.log('Removing.');
             $('.sidebar .clanek-nahled:last-child').remove();
         } else {
-            console.log('Finished.');
             break;
         }
     }
