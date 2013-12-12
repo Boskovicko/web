f3a64c24e225674549a020d45c65092a29433a00
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2013-12-12 13:47:21 +0100

    Lepší kód pro vyvažování délky sidebaru.

--- js/balanced-sidebar.js
+++ js/balanced-sidebar.js
@@ -1,12 +1,19 @@
-$(document).ready(function() {
+$(window).load(function() {
+    var MIN_RELATED_ARTICLES = 1;
+    var TOLERABLE_OVERHANG = 150;
     var mainColumnHeight = $('.clanek-telo').height();
+    console.log('Main column height: ' + mainColumnHeight);
     for (;;) {
-        var MIN_RELATED_ARTICLES = 1;
         var sidebarHeight = $('.sidebar').height();
         var relatedArticleCount = $('.sidebar .clanek-nahled').length;
-        if (sidebarHeight > mainColumnHeight && relatedArticleCount > MIN_RELATED_ARTICLES) {
+        console.log('Sidebar: ' + sidebarHeight);
+        console.log('Related articles: ' + relatedArticleCount);
+        if ((sidebarHeight > mainColumnHeight + TOLERABLE_OVERHANG)
+            && (relatedArticleCount > MIN_RELATED_ARTICLES)) {
+            console.log('Removing.');
             $('.sidebar .clanek-nahled:last-child').remove();
         } else {
+            console.log('Finished.');
             break;
         }
     }
