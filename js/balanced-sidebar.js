7e07093c2b1aa87c922ab3d83b501ecf346c3ad5
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2013-12-17 12:33:57 +0100

    Sjednotil jsem CSS do angličtiny.

--- js/balanced-sidebar.js
+++ js/balanced-sidebar.js
@@ -1,13 +1,13 @@
 $(window).load(function() {
     var MIN_RELATED_ARTICLES = 1;
     var TOLERABLE_OVERHANG = 150;
-    var mainColumnHeight = $('.clanek-telo').height();
+    var mainColumnHeight = $('.article-container').height();
     for (;;) {
         var sidebarHeight = $('.sidebar').height();
-        var relatedArticleCount = $('.sidebar .clanek-nahled').length;
+        var relatedArticleCount = $('.sidebar .article-preview').length;
         if ((sidebarHeight > mainColumnHeight + TOLERABLE_OVERHANG)
             && (relatedArticleCount > MIN_RELATED_ARTICLES)) {
-            $('.sidebar .clanek-nahled:last-child').remove();
+            $('.sidebar .article-preview:last-child').remove();
         } else {
             break;
         }
