34c74eb8bb76657065cd64e82e4ac0276ede094d
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2014-01-29 19:06:06 +0100

    Přidal jsem kód pro střídání reklamních bannerů.

---
+++ ads/list.js
@@ -0,0 +1,14 @@
+[
+    {
+        "name": "25 let Boskovicka",
+        "image": "/ads/narozeniny.png",
+        "text": "Už čtvrt století díky nám víte, co se v regionu děje.",
+        "url": "/clanky/2014/01/11/zpravodajstvi-prinasime-uz-ctvrt-stoleti.html"
+    },
+    {
+        "name": "Waldorfská školka",
+        "image": "/ads/skolka.png",
+        "text": "Waldorfská školka v Boskovicích?",
+        "url": "/clanky/2014/01/11/waldorfska-skolka-v-boskovicich.html"
+    }
+]
