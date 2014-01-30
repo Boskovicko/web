0122cfeec4ee9489498449363692050f796661b5
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2014-01-30 11:34:10 +0100

    Podpora ne-širokoúhlých obrázků na titulce.

---
+++ js/cover-photos.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.article-preview img').each(function(i, preview) {
+        $(preview).load(function() {
+            var image = $(this);
+            var aspect = image.width()/image.height();
+            if (aspect < 1.9) {
+                $(preview).parents('.article-preview').addClass('low-aspect');
+            }
+        });
+    });
+});
