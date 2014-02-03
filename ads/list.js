144f27dbdec95e40fdc8c6a9a5c251688048644f
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2014-02-03 13:15:00 +0100

    Přidal jsem banner na rozhovor s Martinem Krajíčkem.

--- ads/list.js
+++ ads/list.js
@@ -10,5 +10,11 @@
         "image": "/ads/skolka.png",
         "text": "Waldorfská školka v Boskovicích?",
         "url": "/clanky/2014/01/11/waldorfska-skolka-v-boskovicich.html"
+    },
+    {
+        "name": "Rozhovor s M. Krajíčkem",
+        "image": "/ads/krajda.png",
+        "text": "Rozhovor s Martinem Krajíčkem",
+        "url": "/clanky/2014/01/28/rozhovor-s-martinem-krajickem.html"
     }
 ]
