5985091dec48efc6b3630d5ca22b8818249a1981
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2014-07-12 10:33:02 +0200

    Pročistil jsem upoutávky na témata.

--- ads/upoutavky.js
+++ ads/upoutavky.js
@@ -10,33 +10,8 @@
         "url": "/temata/sportovni-hala/"
     },
     {
-        "name": "Téma: Hazard",
-        "text": "Přečtěte si → Hazard na Boskovicku",
-        "url": "/temata/hazard"
-    },
-    {
-        "name": "Zdražení haly",
-        "text": "Videoznáznam → Sportovní hala se prodraží o 6,6 miliónu",
-        "url":  "/clanky/2014/05/02/jednani-zastupitelstva-o-sportovni-hale.html"
-    },
-    {
-        "name": "Narozeniny",
-        "text": "Přečtěte si → 25 let Boskovicka",
-        "url": "/temata/25-let-boskovicka/"
-    },
-    {
-        "nazev": "Anketa Panský dvůr",
-        "text": "Zajímá vás budoucnost Panského dvora? Vyplňte anketu!",
-        "url": "http://goo.gl/38zpqB"
-    },
-    {
-        "nazev": "Téma: Školky",
-        "text": "Přečtěte si → Mateřské školky na Boskovicku",
-        "url": "/temata/skolky/"
-    },
-    {
         "nazev": "Fotky z festivalu",
-        "text": "Podívejte se na fotky z festivalu Boskovice 2014 →",
+        "text": "Podívejte se na fotky z festivalu Boskovice 2014",
         "url": "https://www.facebook.com/media/set/?set=a.10152496178873632.1073741841.231822703631&type=1"
     }
 ]
