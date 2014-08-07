f3e4e0d3996b2110701fc54ea743c6c1e370007a
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2014-08-07 09:14:07 +0200

    Stáhl jsem upoutávky na fotky z fesťáku.

--- ads/list.js
+++ ads/list.js
@@ -6,12 +6,6 @@
         "url": "http://www.autecobs.cz/"
     },
     {
-        "name": "Festival",
-        "image": "/ads/festival.jpg",
-        "text": "Festival Boskovice 2014",
-        "url": "https://www.facebook.com/media/set/?set=a.10152496178873632.1073741841.231822703631&type=1"
-    },
-    {
         "name": "COOP Boskovice",
         "image": "/ads/coop.png",
         "text": "COOP Boskovice",
--- ads/upoutavky.js
+++ ads/upoutavky.js
@@ -8,10 +8,5 @@
         "name": "Téma: Hala",
         "text": "Přečtěte si → Sportovní hala Slovákova",
         "url": "/temata/sportovni-hala/"
-    },
-    {
-        "nazev": "Fotky z festivalu",
-        "text": "Prohlédněte si fotky z festivalu Boskovice 2014",
-        "url": "https://www.facebook.com/media/set/?set=a.10152496178873632.1073741841.231822703631&type=1"
     }
 ]
