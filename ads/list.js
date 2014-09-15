3c09cd843c4535de1d01f4d6e7e7c22cc37990d8
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2014-09-15 08:08:46 +0200

    Přidal jsem banner na předvolební rozhovory.

--- ads/list.js
+++ ads/list.js
@@ -1,14 +1,8 @@
 [
     {
-        "name": "Auteco BS",
-        "image": "/ads/auteco.png",
-        "text": "Auteco BS: Autorizovaný servis Renault",
-        "url": "http://www.autecobs.cz/"
-    },
-    {
-        "name": "COOP Boskovice",
-        "image": "/ads/coop.png",
-        "text": "COOP Boskovice",
-        "url": "http://www.jednotabce.cz/promo-akce.php"
+        "name": "Volby trailer",
+        "image": "/ads/trailer.png",
+        "text": "Podívejte se na trailer k volebním rozhovorům",
+        "url": "https://vimeo.com/105990612"
     }
 ]
