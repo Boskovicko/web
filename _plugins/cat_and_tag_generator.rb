dcc6d3aaec3e4892ce25719d376da44c23dc4e81
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2014-10-08 19:41:26 +0200

    Aktualizace stránkovacího pluginu po přechodu na nového Jekylla.

--- _plugins/cat_and_tag_generator.rb
+++ _plugins/cat_and_tag_generator.rb
@@ -38,9 +38,9 @@ module Jekyll
     end
 
     def paginate(site, type, posts)
-      pages = Pager.calculate_pages(posts[1], site.config['paginate'].to_i)
+      pages = Jekyll::Paginate::Pager.calculate_pages(posts[1], site.config['paginate'].to_i)
       (1..pages).each do |num_page|
-        pager = Pager.new(site, num_page, posts[1], pages)
+        pager = Jekyll::Paginate::Pager.new(site, num_page, posts[1], pages)
         path = "/#{type}/#{posts[0]}"
         if num_page > 1
           path = path + "/strana/#{num_page}"
